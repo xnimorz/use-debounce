443812c253b29ef701e372c38208dac2cee16b6b
Nik Mostovoy <nik.mostovoy@gmail.com>
2019-05-17 17:45:48 +0300

    useCallback to cacheTrigger

--- src/cache.js
+++ src/cache.js
@@ -3,7 +3,7 @@ const useDebouncedCallback = require('./callback').default;
 
 export default function useDebounce(value, delay, options = {}) {
   const [state, dispatch] = React.useState(value);
-  const [callback, cancel] = useDebouncedCallback((value) => dispatch(value), delay, options);
+  const [callback, cancel] = useDebouncedCallback(React.useCallback((value) => dispatch(value), []), delay, options);
 
   React.useEffect(() => {
     if (state !== value) {

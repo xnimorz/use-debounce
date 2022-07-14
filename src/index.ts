ab19d2eee48ecbd8263e475e3809c2a75a1ed48d
Tryggvi Gylfason <tryggvi@spotify.com>
2022-07-14 14:07:11 +0200

    export types from package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import useDebounce from './useDebounce';
-import useDebouncedCallback from './useDebouncedCallback';
+import useDebouncedCallback, { CallOptions, ControlFunctions, DebouncedState, Options } from './useDebouncedCallback';
 import useThrottledCallback from './useThrottledCallback';
 
-export { useDebounce, useDebouncedCallback, useThrottledCallback };
+export {
+  useDebounce,
+  useDebouncedCallback,
+  useThrottledCallback,
+  CallOptions,
+  ControlFunctions,
+  DebouncedState,
+  Options,
+};

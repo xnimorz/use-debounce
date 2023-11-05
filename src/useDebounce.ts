7d15997808cbe4ccd927b6dbdb995ae2d94b9b5b
Nik <nik@xnim.me>
2023-11-05 15:50:13 +0000

    Replace useState with useReducer

--- src/useDebounce.ts
+++ src/useDebounce.ts
@@ -1,23 +1,12 @@
-import { useCallback, useRef, useState, Dispatch } from 'react';
+import { useCallback, useRef, useReducer } from 'react';
 import useDebouncedCallback, { DebouncedState } from './useDebouncedCallback';
 
 function valueEquality<T>(left: T, right: T): boolean {
   return left === right;
 }
 
-function adjustFunctionValueOfSetState<T>(value: T): T | (() => T) {
-  return typeof value === 'function' ? () => value : value;
-}
-
-function useStateIgnoreCallback<T>(initialState: T): [T, Dispatch<T>] {
-  const [state, setState] = useState(
-    adjustFunctionValueOfSetState(initialState)
-  );
-  const setStateIgnoreCallback = useCallback(
-    (value: T) => setState(adjustFunctionValueOfSetState(value)),
-    []
-  );
-  return [state, setStateIgnoreCallback];
+function reducer<T>(_: T, action: T) {
+  return action;
 }
 
 export default function useDebounce<T>(
@@ -32,7 +21,7 @@ export default function useDebounce<T>(
 ): [T, DebouncedState<(value: T) => void>] {
   const eq = (options && options.equalityFn) || valueEquality;
 
-  const [state, dispatch] = useStateIgnoreCallback(value);
+  const [state, dispatch] = useReducer(reducer, value);
   const debounced = useDebouncedCallback(
     useCallback((value: T) => dispatch(value), [dispatch]),
     delay,
@@ -45,5 +34,5 @@ export default function useDebounce<T>(
     previousValue.current = value;
   }
 
-  return [state, debounced];
+  return [state as T, debounced];
 }
